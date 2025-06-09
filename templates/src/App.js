00c63f2d4254c083e1df795176ec080ec0ba761d
Deepthi <deepthiyarram@gmail.com>
2025-06-09 16:54:16 +0530

    QR code

---
+++ templates/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import QR from './slides/QR.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <QR/>
+    </div>
+  );
+}
+
+export default App;
